a8bdb3aafb8d6edd9c1576642709303e6f673546
Joaquín <joaquin.villalon@mail.udp.cl>
2020-10-16 22:02:53 -0300

    se modifica match en decrypt.js

--- decrypt.js
+++ decrypt.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        http://127.0.0.1:5000/
+// @match        https://htmlpreview.github.io/?https://github.com/Villa10n/tarea-3-criptografia/blob/main/index.html
 // @grant        none
 // @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js
 // ==/UserScript==
